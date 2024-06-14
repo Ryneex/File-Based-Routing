1815b365ea9a6a35a73cf257c2aa7a47f06a2c47
MI Sajid <misajidsa@gmail.com>
2024-06-14 06:02:07 +0600

    fix: send stream instead of text

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -2,5 +2,5 @@ import { type FastifyRequest, type FastifyReply } from "fastify";
 import fs from "fs";
 
 export function GET(req: FastifyRequest, res: FastifyReply) {
-    res.type("html").send(fs.readFileSync(process.cwd() + "/src/html/index.html", "utf-8"));
+    res.type("html").send(fs.createReadStream(process.cwd() + "/src/html/index.html", "utf-8"));
 }
